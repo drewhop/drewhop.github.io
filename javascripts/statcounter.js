ca3e86c41cd29690abb0737a702c9364f6bd2b2a
drewhop <drewhopskotch@yahoo.com>
2013-07-26 21:14:26 -0500

    Create statcounter.js

---
+++ javascripts/statcounter.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+var sc_project=9114352; 
+var sc_invisible=1; 
+var sc_security="f55f8078"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter_xhtml.js'></"+"script>");
+//]]>
